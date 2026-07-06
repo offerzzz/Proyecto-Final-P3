76a62f701c03e8bdfeb555ace425269c73ab7caf
Matias677 <matias.ortiz.canar@udla.edu.ec>
2026-07-05 20:12:49 -0500

    Agregue funciones registrarTurno con validacion y listarRegistros

--- ProyectoP3.c
+++ ProyectoP3.c
@@ -52,3 +52,38 @@ void guardarCSV(RegistroProduccion *arreglo, int total) {
     fclose(archivo);
     printf("Datos guardados exitosamente en produccion_turnos.csv\n");
 }
+void listarRegistros(RegistroProduccion *arreglo, int total) {
+    printf("\n--- LISTADO DE REGISTROS ---\n");
+    for (int i = 0; i < total; i++) {
+        float rend = calcularRendimiento(arreglo[i].materia_prima_kg, arreglo[i].desperdicio_kg);
+        printf("Cod: %s | Fecha: %s | CC: %s | Turno: %d | MP: %.1f kg | Desp: %.1f kg | Rend: %.2f%%\n",
+               arreglo[i].codigo_registro, arreglo[i].fecha, arreglo[i].centro_costo,
+               arreglo[i].turno, arreglo[i].materia_prima_kg, arreglo[i].desperdicio_kg, rend);
+            }
+}
+
+void registrarTurno(RegistroProduccion *arreglo, int *total) {
+    if (*total >= 100) {
+        printf("Error: Capacidad maxima de registros alcanzada.\n");
+        return;
+    }
+    printf("Ingrese Codigo de Registro: ");
+    scanf("%15s", arreglo[*total].codigo_registro); 
+    printf("Ingrese Fecha (DD/MM/AAAA): ");
+    scanf("%10s", arreglo[*total].fecha);
+    printf("Ingrese Centro de Costo: ");
+    scanf("%19s", arreglo[*total].centro_costo);
+    printf("Ingrese Turno (1, 2, 3): ");
+    scanf("%d", &arreglo[*total].turno);
+    printf("Ingrese Materia Prima (kg): ");
+    scanf("%f", &arreglo[*total].materia_prima_kg);
+    printf("Ingrese Desperdicio (kg): ");
+    scanf("%f", &arreglo[*total].desperdicio_kg);
+    printf("Ingrese Tiempo (min): ");
+    scanf("%d", &arreglo[*total].tiempo_min);
+    printf("Ingrese Unidades: ");
+    scanf("%d", &arreglo[*total].unidades_producidas);
+    
+    (*total)++;
+    printf("Registro exitoso!\n");
+}
